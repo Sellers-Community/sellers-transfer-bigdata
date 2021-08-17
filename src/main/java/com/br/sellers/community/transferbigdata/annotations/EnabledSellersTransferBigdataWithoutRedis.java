9f8928384718c1c0dbdc7015cfae05ea968663b1
Marcelo Vieira <marcelo.vieira@evoluum.com.br>
2021-08-17 10:44:19 -0300

    ad nova anotação

---
+++ src/main/java/com/br/sellers/community/transferbigdata/annotations/EnabledSellersTransferBigdataWithoutRedis.java
@@ -0,0 +1,18 @@
+package com.br.sellers.community.transferbigdata.annotations;
+
+import com.br.sellers.community.transferbigdata.config.RedisCacheConfig;
+import com.br.sellers.community.transferbigdata.service.CreateTransferBigdataService;
+import com.br.sellers.community.transferbigdata.service.GetTransferBigdataService;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@AutoConfigurationPackage
+@Import({CreateTransferBigdataService.class, GetTransferBigdataService.class})
+public @interface EnabledSellersTransferBigdataWithoutRedis {
+}
