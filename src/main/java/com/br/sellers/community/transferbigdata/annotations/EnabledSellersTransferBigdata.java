9d64130ac81910f3a9c8c5c788ae943ad8b69950
Marcelo de Souza Vieira <marcelosouzav@gmail.com>
2022-05-02 14:32:38 -0300

    add delete key

--- src/main/java/com/br/sellers/community/transferbigdata/annotations/EnabledSellersTransferBigdata.java
+++ src/main/java/com/br/sellers/community/transferbigdata/annotations/EnabledSellersTransferBigdata.java
@@ -2,7 +2,9 @@ package com.br.sellers.community.transferbigdata.annotations;
 
 import com.br.sellers.community.transferbigdata.config.RedisCacheConfig;
 import com.br.sellers.community.transferbigdata.service.CreateTransferBigdataService;
+import com.br.sellers.community.transferbigdata.service.DeleteTransferBigdataService;
 import com.br.sellers.community.transferbigdata.service.GetTransferBigdataService;
+import com.br.sellers.community.transferbigdata.service.GetTransferBigdataWithoutDeleteService;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.Import;
 
@@ -13,6 +15,7 @@ import java.lang.annotation.*;
 @Documented
 @Inherited
 @AutoConfigurationPackage
-@Import({RedisCacheConfig.class, CreateTransferBigdataService.class, GetTransferBigdataService.class})
+@Import({RedisCacheConfig.class, CreateTransferBigdataService.class, GetTransferBigdataService.class,
+        GetTransferBigdataWithoutDeleteService.class, DeleteTransferBigdataService.class})
 public @interface EnabledSellersTransferBigdata {
 }
